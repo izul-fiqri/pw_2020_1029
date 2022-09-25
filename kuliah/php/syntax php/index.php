92c377f16c1d8957d0461eb3b4c3f8e25758616b
izul-fiqri <izulfiqri98@gmail.comm>
2022-09-25 13:59:48 +0700

    menambahkan belajar syntax php

---
+++ kuliah/php/syntax php/index.php
@@ -0,0 +1,42 @@
+<?php
+// sintaks php
+
+//Standar Output
+//echo, print
+//print_r
+//var_dump 
+
+// echo 'moh. izul fiqri';
+// print 'moh. izul fiqri';
+// print_r("moh. izul fiqri");
+// var_dump("moh. izul fiqri");
+
+
+// Penulisan Sintaks PHP
+// 1. PHP di dalam HTML
+// 2. HTML di dalam PHP
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Belajar PHP</title>
+</head>
+
+<body>
+  <!-- php dalam html -->
+  <!-- <h1>Halo, Selamat datang <?php echo "izul" ?></h1>
+  <p><?php echo "ini adalah paragraf" ?></p> -->
+  //html dalam php
+  <?php
+  echo " <h1>Halo, Selamat datang</h1>"
+  ?>
+
+</body>
+
+</html>
